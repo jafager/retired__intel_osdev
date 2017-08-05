aa3827a13c98a73f0138d2866b5bf82c15b5fb76
Jason A. Fager <jafager@jafager.com>
2017-08-05 09:22:55 -0400

    added multiboot check

--- second/startup.s
+++ second/startup.s
@@ -1,7 +1,37 @@
 .text
 
+
+
 .global startup
 startup:
 
+    movl $stack_top, %esp
+
+    call check_multiboot
+
     movl $0x2f4b2f4f, 0xb8000
+    hlt
+
+
+
+check_multiboot:
+
+    cmp $0x36d76289, %eax
+    jne check_multiboot_failed
+    ret
+
+    check_multiboot_failed:
+
+        movb '0', %al
+        jmp panic
+        
+
+
+
+panic:
+
+    movl $0x4f524f45, 0xb8000
+    movl $0x4f3a4f52, 0xb8004
+    movl $0x4f204f20, 0xb8008
+    movb %al,         0xb800a
     hlt
